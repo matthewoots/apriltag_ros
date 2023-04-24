0a638f3dac9bf4aa96300c1bc57cb3f47f84a9c4
matthewoots <matthewoots@gmail.com>
2023-04-24 15:46:54 +0800

    added compressed calibration data, added blacklist for cf id cameras that should not be used

--- launch/cf_camera_april.py
+++ launch/cf_camera_april.py
@@ -19,17 +19,29 @@ def generate_launch_description():
         get_package_share_directory('apriltag_ros'),
         'cfg', 'tags_36h11.yaml')
 
+    # load blacklist
+    blacklist_yaml = os.path.join(
+        get_package_share_directory('apriltag_ros'),
+        'cfg', 'blacklist.yaml')
+    
     with open(cfg_yaml, 'r') as ymlfile:
         cfg = yaml.safe_load(ymlfile)
     
     with open(crazyflies_yaml, 'r') as ymlfile:
         cf = yaml.safe_load(ymlfile)
 
+    with open(blacklist_yaml, 'r') as ymlfile:
+        bll = yaml.safe_load(ymlfile)
+
     robots_list = cf['robots']
+    blacklist = bll['blacklist_ids']
 
     ld = LaunchDescription()
 
     for x in robots_list.keys():
+        # do not launch camera node for these in the rejected list
+        if x in blacklist:
+            continue
         # load calibration
         calibration_yaml = os.path.join(
             get_package_share_directory('apriltag_ros'),
